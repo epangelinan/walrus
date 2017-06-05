29a31b9dff71a5634f9b6648155cb51599fbc662
Esvethlynna Pangelinan and Joe Kramer <esvethlynna@>
2017-06-05 11:50:49 -0700

    create walrus page using forEach function to display the element when clicking it

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  var elements = ["header", "paragraph", "image"];
+  elements.forEach(function(element) {
+    $("."+ element).click(function() {
+      alert("This is a " + element);
+    });
+  });
+});
